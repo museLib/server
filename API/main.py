de1a7aa0a14bb43bdff73c35bf9ca4f65c72604b
Hiroki.T <hirodora@me.com>
2020-04-09 23:29:22 +0900

    add: mvc

--- API/main.py
+++ API/main.py
@@ -1,8 +1,5 @@
-from fastapi import FastAPI
+from urls import app
+import uvicorn
 
-app = FastAPI()
-
-
-@app.get('/')
-async def root():
-    return {'message': 'Hello, World'}
+if __name__ == "__main__":
+    uvicorn.run(app=app, host="0.0.0.0", port=8000)
---
+++ API/urls.py
@@ -0,0 +1,5 @@
+from controller import *
+
+
+app.add_api_route('/', index)
+
