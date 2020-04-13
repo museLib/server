222a7a19171fef374dce801841172cea5f0e5f26
Hiroki.T <hirodora@me.com>
2020-04-13 13:11:29 +0900

    add: introduced PGroonga

--- API/migrate.py
+++ API/migrate.py
@@ -1,7 +1,13 @@
 from db import db
 from user import models
 from book import models
+from sqlalchemy.sql import text
 
 
 if __name__ == "__main__":
     db.Base.metadata.create_all(db.engine)
+
+    # Install pgroonga
+    q = text('CREATE EXTENSION pgroonga')
+    session = db.Session()
+    session.execute(q)
